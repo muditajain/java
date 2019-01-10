47556073721a35548afd63e2d9f9d73fe60b6172
mudita <mudita18csu132@ncuindia.edu>
2019-01-10 18:23:24 +0530

    DisplayHello is updated

--- DisplayHello.java
+++ DisplayHello.java
@@ -1,11 +1,10 @@
 // Printing a line with multiple statements
 // Program name must be same as Main class name 
-class DisplayHello {
-public static void main( String args[] )
+class DisplayHello
 {
-System.out.print( “Hello " );
-System.out.println( “Hello!" );
-
-
+           public static void main(String args[])
+{
+System.out.print("Hello");
+System.out.println("Hello!");
 }
 }

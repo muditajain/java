8d75b0a7344d4f54f3340024c4374ce30e40ee78
mudita <mudita18csu132@ncuindia.edu>
2019-01-10 23:17:01 +0530

    card is ready

---
+++ card.java
@@ -0,0 +1,30 @@
+procedural
+
+int main (){
+Card card;
+Add s.no(card,45);
+Add name(card,45);
+Add rollno.(card,45);
+Add subject(card,6);
+Add grades(card,6);
+std::cout<<"total grade of students\n";
+return 0;
+}
+
+
+object oriented;
+int main(){
+Card card(45)
+S.no s.no(45)
+Name name (45)
+Rollno. rollno.(45)
+Subject subject(6)
+Grades grades(6)
+card.add(s.no);
+card.add(name);
+card.add(rollno.);
+card.add(subject);
+card.add(grade);
+std::cout<<"total grade of student\n";
+return 0;
+}

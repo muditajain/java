fbf24d96bcc5b6d26d1b58adcffd043487601be9
mudita <mudita18csu132@ncuindia.edu>
2019-01-11 16:23:29 +0530

    numbers are added

---
+++ numbers.java
@@ -0,0 +1,13 @@
+class Addnumbers
+{
+public static void main(String args[])
+{
+int x=10;
+int y=10;
+int z;
+System.out.println("enter two integers to calculate their sum");
+z=x+y;
+System.out.println("sum of integers="+z);
+}
+}
+

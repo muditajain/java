5ec485298bc341c3a953d7d0a39e7ddce7b0c142
mudita <mudita18csu132@ncuindia.edu>
2019-01-11 17:29:46 +0530

    conversion is done

---
+++ conversion.java
@@ -0,0 +1,9 @@
+class minute {
+public static void main(String args[]){
+System.out.println("enter the number of minutes:");
+int year = minutes/525600;
+int day =minutes/1440;
+int remainingMinutes = day % 525600;
+System.out.println(minutes +"minutes is "+ year +" years and "+remainingMinutes + "days");
+}
+}
